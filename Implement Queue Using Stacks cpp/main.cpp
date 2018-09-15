22ce27213625ffa365bf08aa438c9b090e100a77
Nirob Hasan <37204883+NirobxHasan@users.noreply.github.com>
2018-09-15 14:48:54 +0600

    Create main.cpp

---
+++ Implement Queue Using Stacks cpp/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "QueueUsingStacks.h"
+using namespace std;
+int value;
+int main(int argc, char *argv[])
+{
+    QueueUsingStacks que;
+    cout<<"STACK"<<endl;
+    cout<<endl;
+    if(que.isEmpty())
+        cout<<"Queue is empty."<<endl;
+    que.dequeue(value);
+    cout<<endl;
+    que.enqueue(103);
+    que.enqueue(105);
+    que.enqueue(10);
+    que.enqueue(11);
+    que.dequeue(value);
+    que.enqueue(9);
+    cout<<"size: "<<que.size()<<endl;
+    que.makeEmpty();
+    if(que.isEmpty())
+        cout<<"Queue is empty."<<endl;
+    cout<<"size: "<<que.size()<<endl;
+    cout<<endl;
+    que.enqueue(10);
+    que.enqueue(2);
+    que.dequeue(value);
+    if(que.dequeue(value)) cout<<"Queue is empty."<<endl;
+    que.dequeue(value);
+    cout<<endl;
+    return 0;
+}
